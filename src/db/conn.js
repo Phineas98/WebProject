c71a006d3b3534459d61528d2fd1e00bde11fea7
phineas <prashantsharma9389@gmail.com>
2021-08-03 16:10:45 +0530

    name

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/DanceRegistration', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/registrationform', {
   useNewUrlParser:true,
   useUnifiedTopology:true,
   useCreateIndex:true
